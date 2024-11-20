6d70c028f45b8d4209493a04991d1ad9d00569d0
Benjamin <benjamin.giraud-renard@student.tuwien.ac.at>
2024-11-20 17:17:56 +0100

    Add default shadcn vue + vite project

---
+++ mcm-frontend/src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import {Button} from "@/components/ui/button"
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">{{ msg }}</h1>
+  <Button @click="count++">Shadcn Button style</Button>
+  <p>count is: {{ count }}</p>
+</template>

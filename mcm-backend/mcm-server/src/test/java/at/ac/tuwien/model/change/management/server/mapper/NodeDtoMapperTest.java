f5eee1fa1bdcdb8c49f1f448c15e79b35b51d9d8
TheoHauray <99470952+TheoHauray@users.noreply.github.com>
2024-11-21 16:28:11 +0100

    factorize tests & remove unused imports

--- mcm-backend/mcm-server/src/test/java/at/ac/tuwien/model/change/management/server/mapper/NodeDtoMapperTest.java
+++ mcm-backend/mcm-server/src/test/java/at/ac/tuwien/model/change/management/server/mapper/NodeDtoMapperTest.java
@@ -1,12 +1,9 @@
 package at.ac.tuwien.model.change.management.server.mapper;
 
-import at.ac.tuwien.model.change.management.core.model.Model;
 import at.ac.tuwien.model.change.management.core.model.Node;
 import at.ac.tuwien.model.change.management.core.model.Relation;
 import at.ac.tuwien.model.change.management.core.model.UMLetPosition;
-import at.ac.tuwien.model.change.management.server.dto.ModelDTO;
 import at.ac.tuwien.model.change.management.server.dto.NodeDTO;
-import at.ac.tuwien.model.change.management.server.dto.RelationDTO;
 import org.junit.jupiter.api.Test;
 import org.mapstruct.factory.Mappers;
 
@@ -19,22 +16,27 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 public class NodeDtoMapperTest {
 
     private final NodeDtoMapper mapper = Mappers.getMapper(NodeDtoMapper.class);
+    private final String ID = "1";
+    private final String TYPE = "type";
+    private final String TEXT = "test";
+    private final Set<String> LABELS = Set.of("label");
+    private final UMLetPosition UMLET_POSITION = new UMLetPosition(1, 1, 1, 1);
+    private final Map<String, Object> PROPERTIES = Map.of("key", new Object());
 
     @Test
     void testToDto() {
         Node node = new Node();
 
-        node.setId("1");
-        node.setType("type");
-        node.setText("text");
-        node.setLabels(Set.of("label"));
+        node.setId(ID);
+        node.setType(TYPE);
+        node.setText(TEXT);
+        node.setLabels(LABELS);
         node.setRelations(Set.of(new Relation()));
-        node.setProperties(Map.of("key", new Object()));
-        node.setUmletPosition(new UMLetPosition(1, 1, 1, 1));
+        node.setProperties(PROPERTIES);
+        node.setUmletPosition(UMLET_POSITION);
 
         NodeDTO nodeDTO = mapper.toDto(node);
 
-        assertNotNull(nodeDTO);
         assertNotNull(nodeDTO);
         assertEquals(node.getId(), nodeDTO.id());
         assertEquals(node.getType(), nodeDTO.type());
@@ -46,16 +48,14 @@ public class NodeDtoMapperTest {
 
     @Test
     void testFromDto() {
-        UMLetPosition umLetPosition = new UMLetPosition(1, 1, 1, 1);
-
         NodeDTO nodeDTO = new NodeDTO(
-                "1",
-                "text",
+                ID,
+                TEXT,
                 Set.of(),
-                "type",
-                Map.of("key", "value"),
-                Set.of("label"),
-                umLetPosition
+                TYPE,
+                PROPERTIES,
+                LABELS,
+                UMLET_POSITION
         );
 
         Node node = mapper.fromDto(nodeDTO);

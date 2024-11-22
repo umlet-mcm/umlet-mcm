7862f3d344657dc978c2704297bd90e64a238ed4
Dániel Hajós <e12432109@student.tuwien.ac.at>
2024-11-22 13:29:33 +0000

    Model classes implementation

---
+++ mcm-backend/mcm-core/src/main/java/at/ac/tuwien/model/change/management/core/model/RelativePosition.java
@@ -0,0 +1,18 @@
+package at.ac.tuwien.model.change.management.core.model;
+
+import lombok.Getter;
+
+@Getter
+public class RelativePosition {
+    private int absX;
+    private int absY;
+    private int offsetX;
+    private int offsetY;
+
+    public RelativePosition(int offsetX, int offsetY, int refX, int refY) {
+        this.offsetX = offsetX;
+        this.offsetY = offsetY;
+        this.absX = refX + offsetX;
+        this.absY = refY + offsetY;
+    }
+}

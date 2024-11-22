014eb19215afd9976efb9c55c314cb3b6a71e153
12432109 <e12432109@student.tuwien.ac.at>
2024-11-22 11:12:20 +0100

    Remove uxf and parsing related classes

--- mcm-backend/mcm-core/src/main/java/at/ac/tuwien/model/change/management/core/model/BaseAttributes.java
+++ mcm-backend/mcm-core/src/main/java/at/ac/tuwien/model/change/management/core/model/BaseAttributes.java
@@ -1,6 +1,5 @@
 package at.ac.tuwien.model.change.management.core.model;
 
-import at.ac.tuwien.model.change.management.core.model.attributes.AttributeKeys;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,28 +12,22 @@ import java.util.Map;
 @Getter
 @Setter
 public abstract class BaseAttributes {
+    @Nullable
+    protected String id;
     protected String description;
     protected Map<String, Object> mcmAttributes;
-    private String mcmType = null;
 
-    @Nullable
-    public String getMcmId() {
-        return (String) mcmAttributes.get(AttributeKeys.ID);
-    }
-
+    // Finding the type of an element requires a custom logic so a custom getter is implemented.
     @Nullable
     public String getMcmType() {
-        if (mcmType != null) {
-            return mcmType;
-        }
-
         // find the type key in the attributes
         for (var a : mcmAttributes.entrySet()) {
             // for compatibility reasons, older uxf files use keys like "PPR type", "PPR UVL type"
             if (a.getKey().toLowerCase().contains("type")) {
-                mcmType = (String) a.getValue();
+                return (String) a.getValue();
             }
         }
-        return mcmType;
+        return null;
     }
+
 }

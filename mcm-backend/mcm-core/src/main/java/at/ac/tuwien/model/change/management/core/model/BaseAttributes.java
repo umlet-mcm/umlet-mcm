c8a255072ddd2bfc033509d4e2f2524db057d162
12432109 <e12432109@student.tuwien.ac.at>
2024-11-22 13:35:27 +0100

    Extract mcmType

--- mcm-backend/mcm-core/src/main/java/at/ac/tuwien/model/change/management/core/model/BaseAttributes.java
+++ mcm-backend/mcm-core/src/main/java/at/ac/tuwien/model/change/management/core/model/BaseAttributes.java
@@ -1,5 +1,6 @@
 package at.ac.tuwien.model.change.management.core.model;
 
+import at.ac.tuwien.model.change.management.core.model.attributes.AttributeKeys;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -15,19 +16,19 @@ public abstract class BaseAttributes {
     @Nullable
     protected String id;
     protected String description;
-    protected Map<String, Object> mcmAttributes;
-
-    // Finding the type of an element requires a custom logic so a custom getter is implemented.
     @Nullable
-    public String getMcmType() {
-        // find the type key in the attributes
+    protected String mcmType;
+    private Map<String, Object> mcmAttributes; // this is private so that the custom setter has be used
+
+    // id and mcmType are stored in the mcmAttributes, this setter automatically populates these two fields
+    public void setMcmAttributes(Map<String, Object> mcmAttributes) {
+        this.mcmAttributes = mcmAttributes;
         for (var a : mcmAttributes.entrySet()) {
             // for compatibility reasons, older uxf files use keys like "PPR type", "PPR UVL type"
             if (a.getKey().toLowerCase().contains("type")) {
-                return (String) a.getValue();
+                this.mcmType = (String) a.getValue();
             }
         }
-        return null;
+        this.id = (String) mcmAttributes.get(AttributeKeys.ID);
     }
-
 }

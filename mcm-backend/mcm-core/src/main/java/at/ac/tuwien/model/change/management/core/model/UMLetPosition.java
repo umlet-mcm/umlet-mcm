e75f7fb434203dc34ce2c9eaba427a80c1e23ecc
12432109 <e12432109@student.tuwien.ac.at>
2024-11-22 13:37:11 +0100

    Remove XML annotations, a separate uxf version will be created

--- mcm-backend/mcm-core/src/main/java/at/ac/tuwien/model/change/management/core/model/UMLetPosition.java
+++ mcm-backend/mcm-core/src/main/java/at/ac/tuwien/model/change/management/core/model/UMLetPosition.java
@@ -1,26 +1,14 @@
 package at.ac.tuwien.model.change.management.core.model;
 
-import jakarta.xml.bind.annotation.XmlAccessType;
-import jakarta.xml.bind.annotation.XmlAccessorType;
-import jakarta.xml.bind.annotation.XmlElement;
-import jakarta.xml.bind.annotation.XmlRootElement;
 import lombok.Getter;
 import lombok.Setter;
-
-// JAXB expects a no args constructor so this record was changed to a class
 
 @Getter
 @Setter
 
-@XmlRootElement(name = "coordinates")
-@XmlAccessorType(XmlAccessType.FIELD)
 public class UMLetPosition {
-    @XmlElement
     int x;
-    @XmlElement
     int y;
-    @XmlElement(name = "w")
     int width;
-    @XmlElement(name = "h")
     int height;
 }

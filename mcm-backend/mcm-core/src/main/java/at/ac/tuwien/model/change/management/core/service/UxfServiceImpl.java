82b20ec385a33c6724defef825392e1ed8617f47
12432109 <e12432109@student.tuwien.ac.at>
2024-11-27 18:24:59 +0100

    Test code for import/export

--- mcm-backend/mcm-core/src/main/java/at/ac/tuwien/model/change/management/core/service/UxfServiceImpl.java
+++ mcm-backend/mcm-core/src/main/java/at/ac/tuwien/model/change/management/core/service/UxfServiceImpl.java
@@ -4,14 +4,17 @@ import at.ac.tuwien.model.change.management.core.mapper.uxf.ModelUxfMapper;
 import at.ac.tuwien.model.change.management.core.model.Configuration;
 import at.ac.tuwien.model.change.management.core.model.Model;
 import at.ac.tuwien.model.change.management.core.model.intermediary.ModelUxf;
+import at.ac.tuwien.model.change.management.core.model.utils.RelationUtils;
 import jakarta.xml.bind.JAXBContext;
 import jakarta.xml.bind.JAXBException;
+import jakarta.xml.bind.Marshaller;
 import jakarta.xml.bind.Unmarshaller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamSource;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -47,7 +50,15 @@ public class UxfServiceImpl implements UxfService {
         try {
             ModelUxf res = (ModelUxf) unmarshaller.unmarshal(input);
             Model m = modelUxfMapper.toModel(res);
-            log.debug(m.toString());
+            Model wr = RelationUtils.processRelations(m);
+
+            ModelUxf b = modelUxfMapper.fromModel(wr);
+
+            Marshaller marshaller = context.createMarshaller();
+            File out = new File( "export.xml" );
+            marshaller.marshal(b, out);
+
+            log.debug(b.toString());
             log.debug(res.toString());
         } catch (JAXBException e) {
             return null;
@@ -55,4 +66,6 @@ public class UxfServiceImpl implements UxfService {
 
         return null;
     }
+
+
 }

baac79b751c7066a65d97063ab6712f9e15c6a0f
Benjamin <benjamin.giraud-renard@student.tuwien.ac.at>
2024-12-02 17:34:18 +0100

    Add tabs full graph vs request

--- mcm-frontend/src/components/main-content/MainContent.vue
+++ mcm-frontend/src/components/main-content/MainContent.vue
@@ -3,7 +3,7 @@ import { Button } from '@/components/ui/button'
 import QueryEditor from "@/components/main-content/QueryEditor.vue"
 import GraphVisualisation from "@/components/main-content/GraphVisualisation.vue"
 import { ref } from "vue"
-import { Play, Eye, ArrowLeft, Save } from 'lucide-vue-next'
+import { Play } from 'lucide-vue-next'
 import {Model} from "@/types/Model.ts";
 import {Node} from "@/types/Node.ts";
 
@@ -25,10 +25,6 @@ const emit = defineEmits(["update:selectedNode"]);
 const executeQuery = () => {
   console.log('Executing query:', query.value)
 }
-
-const previewQuery = () => {
-  console.log('Preview query:', query.value)
-}
 </script>
 
 <template>
@@ -41,31 +37,24 @@ const previewQuery = () => {
           <Play class="mr-2 h-4 w-4" />
           Execute Query
         </Button>
-        <Button @click="previewQuery" variant="secondary" class="flex items-center">
-          <Eye class="mr-2 h-4 w-4" />
-          Preview Query
-        </Button>
       </div>
     </div>
-    <div class="flex items-center justify-between p-4">
-      <h1 class="text-xl font-bold p-4">
+    <div class="flex items-center justify-between">
+      <h1 class="text-lg font-bold p-4">
         Current model : {{ selectedModel?.id }}
       </h1>
-      <div class="flex gap-4">
-        <Button class="p-2 rounded-full" variant="outline">
-          <ArrowLeft class="h-5 w-5" />
-        </Button>
-        <Button class="p-2 rounded-full" variant="outline">
-          <Save class="h-5 w-5" />
-        </Button>
-      </div>
+<!--      <div class="flex p-4">-->
+<!--        <Button class="p-2 rounded-full" variant="destructive">-->
+<!--          <ArrowLeft class="h-5 w-5"/>-->
+<!--          Previous-->
+<!--        </Button>-->
+<!--      </div>-->
     </div>
 
     <div class="flex-1 p-2 overflow-hidden">
       <div v-if="selectedModel" class="h-full w-full">
         <GraphVisualisation
             class="h-full w-full"
-            :selected-node="selectedNode"
             :selected-model="selectedModel"
             @update:selectedNode="emit('update:selectedNode', $event)"/>
       </div>

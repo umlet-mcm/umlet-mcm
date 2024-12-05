a9acfc70034ac8e9555324ea766fa493be7bd049
Lukas Loidolt <e1634039@student.tuwien.ac.at>
2024-12-05 08:02:57 +0100

    complete ConfigurationController

--- mcm-backend/mcm-server/src/main/java/at/ac/tuwien/model/change/management/server/controller/ConfigurationController.java
+++ mcm-backend/mcm-server/src/main/java/at/ac/tuwien/model/change/management/server/controller/ConfigurationController.java
@@ -5,10 +5,9 @@ import at.ac.tuwien.model.change.management.server.dto.ConfigurationDTO;
 import at.ac.tuwien.model.change.management.server.mapper.ConfigurationDtoMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/configurations")
@@ -18,9 +17,33 @@ public class ConfigurationController {
     private final ConfigurationService configurationService;
     private final ConfigurationDtoMapper configurationDtoMapper;
 
+    @GetMapping("/{name}")
+    public ResponseEntity<ConfigurationDTO> getConfiguration(@PathVariable String name) {
+        var configuration = configurationService.getConfigurationByName(name);
+        return ResponseEntity.ok(configurationDtoMapper.toDto(configuration));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ConfigurationDTO>> getAllConfigurations() {
+        var configurations = configurationService.getAllConfigurations();
+        return ResponseEntity.ok(configurationDtoMapper.toDto(configurations));
+    }
+
     @PostMapping
     public ResponseEntity<ConfigurationDTO> createConfiguration(@RequestBody ConfigurationDTO dto) {
-        var createdConfiguration = configurationService.create(configurationDtoMapper.fromDto(dto));
+        var createdConfiguration = configurationService.createConfiguration(configurationDtoMapper.fromDto(dto));
         return ResponseEntity.ok(configurationDtoMapper.toDto(createdConfiguration));
+    }
+
+    @PutMapping
+    public ResponseEntity<ConfigurationDTO> updateConfiguration(@RequestBody ConfigurationDTO dto) {
+        var updatedConfiguration = configurationService.updateConfiguration(configurationDtoMapper.fromDto(dto));
+        return ResponseEntity.ok(configurationDtoMapper.toDto(updatedConfiguration));
+    }
+
+    @DeleteMapping("/{name}")
+    public ResponseEntity<Void> deleteConfiguration(@PathVariable String name) {
+        configurationService.deleteConfiguration(name);
+        return ResponseEntity.noContent().build();
     }
 }
--- mcm-backend/mcm-server/src/main/java/at/ac/tuwien/model/change/management/server/mapper/ConfigurationDtoMapper.java
+++ mcm-backend/mcm-server/src/main/java/at/ac/tuwien/model/change/management/server/mapper/ConfigurationDtoMapper.java
@@ -4,8 +4,11 @@ import at.ac.tuwien.model.change.management.core.model.Configuration;
 import at.ac.tuwien.model.change.management.server.dto.ConfigurationDTO;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface ConfigurationDtoMapper {
     ConfigurationDTO toDto(Configuration configuration);
     Configuration fromDto(ConfigurationDTO dto);
+    List<ConfigurationDTO> toDto(List<Configuration> configurations);
 }

042655e87340cdf7dfbfcd3c3289344b05313a35
Lukas Loidolt <e1634039@student.tuwien.ac.at>
2024-12-06 04:07:09 +0100

    turn all Repository exceptions into unchecked
    in keeping with Spring guidelines

--- mcm-backend/mcm-core/src/main/java/at/ac/tuwien/model/change/management/git/exception/RepositoryAlreadyExistsException.java
+++ mcm-backend/mcm-core/src/main/java/at/ac/tuwien/model/change/management/git/exception/RepositoryAlreadyExistsException.java
@@ -1,6 +1,6 @@
 package at.ac.tuwien.model.change.management.git.exception;
 
-public class RepositoryAlreadyExistsException extends RepositoryCheckedException {
+public class RepositoryAlreadyExistsException extends RepositoryAccessException{
 
     public RepositoryAlreadyExistsException(String msg) {
         super(msg);

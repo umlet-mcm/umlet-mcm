8fbefab1a1a61bd4a96ffcc0e0af499287671a70
Lukas Loidolt <e1634039@student.tuwien.ac.at>
2024-12-06 03:55:14 +0100

    use functional style for accessing JGit Repository where possible

--- mcm-backend/mcm-core/src/main/java/at/ac/tuwien/model/change/management/git/operation/RepositoryManager.java
+++ mcm-backend/mcm-core/src/main/java/at/ac/tuwien/model/change/management/git/operation/RepositoryManager.java
@@ -1,12 +1,46 @@
 package at.ac.tuwien.model.change.management.git.operation;
 
+import lombok.NonNull;
 import org.eclipse.jgit.lib.Repository;
 
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 public interface RepositoryManager {
 
     Repository accessRepository(String repositoryName);
 
     List<Repository> listRepositories();
+
+    default <R> R withRepository(@NonNull String repositoryName, @NonNull Function<Repository, R> function) {
+        try (Repository repository = accessRepository(repositoryName)) {
+            return function.apply(repository);
+        }
+    }
+
+    default void consumeRepository(@NonNull String repositoryName, @NonNull Consumer<Repository> consumer) {
+        try (Repository repository = accessRepository(repositoryName)) {
+            consumer.accept(repository);
+        }
+    }
+
+    default <R> R withAllRepositories(@NonNull Function<List<Repository>, R> function) {
+        List<Repository> repositories = listRepositories();
+        try {
+            return function.apply(repositories);
+        } finally {
+            repositories.forEach(Repository::close);
+        }
+    }
+
+    default void consumeAllRepositories(@NonNull Consumer<List<Repository>> consumer) {
+        List<Repository> repositories = listRepositories();
+        try {
+            consumer.accept(repositories);
+        } finally {
+            repositories.forEach(Repository::close);
+        }
+    }
+
 }
--- mcm-backend/mcm-core/src/main/java/at/ac/tuwien/model/change/management/git/repository/ConfigurationRepositoryImpl.java
+++ mcm-backend/mcm-core/src/main/java/at/ac/tuwien/model/change/management/git/repository/ConfigurationRepositoryImpl.java
@@ -19,8 +19,6 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
-import static at.ac.tuwien.model.change.management.git.util.RepositoryManagerUtils.withRepositories;
-
 @GitComponent
 @Slf4j
 @RequiredArgsConstructor
@@ -46,20 +44,20 @@ public class ConfigurationRepositoryImpl implements ConfigurationRepository {
     @Override
     public Optional<Configuration> findConfigurationByName(@NonNull String name) {
         log.debug("Finding configuration '{}'.", name);
-        try (var repository = repositoryManager.accessRepository(name)) {
+        return repositoryManager.withRepository(name, repository -> {
             if (!RepositoryUtils.repositoryExists(repository)) {
                 log.warn("Repository for configuration '{}' could not be found.", name);
                 return Optional.empty();
             }
             log.debug("Found repository for configuration '{}'.", name);
             return Optional.of(configurationIoManager.readConfigurationFromRepository(repository, Constants.HEAD));
-        }
+        });
     }
 
     @Override
     public List<Configuration> findAllConfigurations() {
         log.debug("Searching all repositories for configurations.");
-        return withRepositories(repositoryManager.listRepositories(), repositories -> {
+        return repositoryManager.withAllRepositories(repositories -> {
             var configurations = repositories.stream()
                     .map(repository -> configurationIoManager.readConfigurationFromRepository(repository, Constants.HEAD))
                     .toList();
@@ -89,16 +87,18 @@ public class ConfigurationRepositoryImpl implements ConfigurationRepository {
     @Override
     public void deleteConfiguration(@NonNull String name) {
         log.debug("Deleting repository for configuration '{}'.", name);
-        try (var repository = repositoryManager.accessRepository(name)) {
-            var pathToWorkDir = repository.getWorkTree().toPath();
-            if (FileSystemUtils.deleteRecursively(pathToWorkDir)) {
-                log.info("Deleted repository for configuration '{}'", name);
-            } else {
-                log.warn("Could not delete repository for configuration '{}', because it does not exist", name);
+        repositoryManager.consumeRepository(name, repository -> {
+            try {
+                var pathToWorkDir = repository.getWorkTree().toPath();
+                if (FileSystemUtils.deleteRecursively(pathToWorkDir)) {
+                    log.info("Deleted repository for configuration '{}'", name);
+                } else {
+                    log.warn("Could not delete repository for configuration '{}', because it does not exist", name);
+                }
+            } catch (IOException e) {
+                throw new RepositoryDeleteException("Failed to delete repository for configuration '" + name + "'.", e);
             }
-        } catch (IOException e) {
-            throw new RepositoryDeleteException("Failed to delete repository for configuration '" + name + "'.", e);
-        }
+        });
     }
 
     private String generateCommitMessage(String name, RepositoryContents<?> repositoryContents) {

42fe2b7a525a259e201a8204794be7cfa5e2e3b5
Lukas Loidolt <mail@lukas-loidolt.at>
2024-12-07 13:00:38 +0100

    remove custom equals and hashCode methods in RelationDTO
    should not be necessary anymore now that we broke the cyclic reference within the DTOs

--- mcm-backend/mcm-server/src/main/java/at/ac/tuwien/model/change/management/server/dto/RelationDTO.java
+++ mcm-backend/mcm-server/src/main/java/at/ac/tuwien/model/change/management/server/dto/RelationDTO.java
@@ -2,7 +2,6 @@ package at.ac.tuwien.model.change.management.server.dto;
 
 import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Objects;
 
 public record RelationDTO(
         String type,
@@ -24,57 +23,4 @@ public record RelationDTO(
         LinkedHashMap<String, String> umletAttributes,
         String pprType
 ) {
-
-
-    // We have to override the default Record implementation of equals and hashcode
-    // because otherwise we have a circular dependency between NodeDTO and RelationDTO
-    // resulting in a StackOverflowError
-
-    // Since the other fields implement a detailed position (expected to be unique)
-    // and the id assigned by the data layer, excepting the target node should be safe
-    @Override
-    public boolean equals(Object o) {
-       if (this == o) return true;
-       if (! (o instanceof RelationDTO that)) return false;
-
-       return Objects.equals(type(), that.type()) &&
-              Objects.equals(umletPosition(), that.umletPosition()) &&
-              Objects.equals(relativeStartPoint(), that.relativeStartPoint()) &&
-              Objects.equals(relativeMidPoints(), that.relativeMidPoints()) &&
-              Objects.equals(relativeEndPoint(), that.relativeEndPoint()) &&
-              Objects.equals(startPoint(), that.startPoint()) &&
-              Objects.equals(endPoint(), that.endPoint()) &&
-              Objects.equals(id(), that.id()) &&
-              Objects.equals(tags(), that.tags()) &&
-              Objects.equals(originalText(), that.originalText()) &&
-              Objects.equals(title(), that.title()) &&
-              Objects.equals(description(), that.description()) &&
-              Objects.equals(mcmAttributes(), that.mcmAttributes()) &&
-              Objects.equals(mcmModel(), that.mcmModel()) &&
-              Objects.equals(mcmModelId(), that.mcmModelId()) &&
-              Objects.equals(umletAttributes(), that.umletAttributes()) &&
-              Objects.equals(pprType(), that.pprType());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(
-                type(),
-                umletPosition(),
-                relativeStartPoint(),
-                relativeMidPoints(),
-                relativeEndPoint(),
-                startPoint(),
-                endPoint(),
-                id(),
-                tags(),
-                originalText(),
-                title(),
-                description(),
-                mcmAttributes(),
-                mcmModel(),
-                mcmModelId(),
-                umletAttributes(),
-                pprType());
-    }
 }

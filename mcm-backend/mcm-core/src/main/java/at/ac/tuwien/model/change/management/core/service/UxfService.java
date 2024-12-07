709170e02d6061d21095254300547e2cabe9f323
12432109 <e12432109@student.tuwien.ac.at>
2024-12-07 20:01:48 +0100

    Export uxf

--- mcm-backend/mcm-core/src/main/java/at/ac/tuwien/model/change/management/core/service/UxfService.java
+++ mcm-backend/mcm-core/src/main/java/at/ac/tuwien/model/change/management/core/service/UxfService.java
@@ -1,6 +1,7 @@
 package at.ac.tuwien.model.change.management.core.service;
 
 import at.ac.tuwien.model.change.management.core.exception.ConfigurationException;
+import at.ac.tuwien.model.change.management.core.exception.ModelNotFoundException;
 import at.ac.tuwien.model.change.management.core.exception.UxfException;
 import at.ac.tuwien.model.change.management.core.model.Configuration;
 import org.springframework.core.io.InputStreamSource;
@@ -10,4 +11,8 @@ public interface UxfService {
     Configuration createConfigurationFromUxf(InputStreamSource file) throws UxfException;
 
     Configuration addUxfToConfiguration(InputStreamSource file, String configurationUUID) throws UxfException, ConfigurationException;
+
+    String exportModel(String modelUuid) throws ModelNotFoundException, UxfException;
+
+    String exportConfiguration(String configurationUuid) throws UxfException;
 }
--- mcm-backend/mcm-server/src/main/java/at/ac/tuwien/model/change/management/server/controller/UxfController.java
+++ mcm-backend/mcm-server/src/main/java/at/ac/tuwien/model/change/management/server/controller/UxfController.java
@@ -1,16 +1,20 @@
 package at.ac.tuwien.model.change.management.server.controller;
 
 import at.ac.tuwien.model.change.management.core.exception.ConfigurationException;
+import at.ac.tuwien.model.change.management.core.exception.ModelNotFoundException;
 import at.ac.tuwien.model.change.management.core.exception.UxfException;
 import at.ac.tuwien.model.change.management.core.model.Configuration;
 import at.ac.tuwien.model.change.management.core.service.ConfigurationNotFoundException;
 import at.ac.tuwien.model.change.management.core.service.UxfService;
 import at.ac.tuwien.model.change.management.server.mapper.ConfigurationDtoMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.charset.StandardCharsets;
 
 @RestController
 @RequestMapping("/api/v1/files/uxf")
@@ -43,10 +47,52 @@ public class UxfController {
                     .body("Configuration with id '" + configId + "' not found");
         } catch (ConfigurationException e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body("Failed to get configuration '" + configId + "': " + e.getMessage());
+                    .body("Failed to get configuration '" + configId + "'");
         } catch (UxfException e) {
             return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY)
-                    .body("Failed to parse uxf: " + e.getMessage());
+                    .body("Failed to parse uxf");
+        }
+    }
+
+    @GetMapping("export/configuration/{configUuid}")
+    public ResponseEntity exportConfiguration(@PathVariable String configUuid) {
+        try {
+            String configXml = uxfService.exportConfiguration(configUuid);
+            byte[] xmlBytes = configXml.getBytes(StandardCharsets.UTF_8);
+            HttpHeaders headers = new HttpHeaders();
+            headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=configuration_" + configUuid + ".uxf");
+            headers.add(HttpHeaders.CONTENT_TYPE, "application/xml");
+            return new ResponseEntity<>(xmlBytes, headers, HttpStatus.OK);
+        } catch (ConfigurationNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Configuration with id '" + configUuid + "' not found");
+        } catch (ConfigurationException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("Failed to get configuration '" + configUuid + "'");
+        } catch (UxfException e) {
+            return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY)
+                    .body("Failed to export configuration to uxf");
+        }
+    }
+
+    @GetMapping("export/model/{modelUuid}")
+    public ResponseEntity exportModel(@PathVariable String modelUuid) {
+        try {
+            String modelXml = uxfService.exportModel(modelUuid);
+            byte[] xmlBytes = modelXml.getBytes(StandardCharsets.UTF_8);
+            HttpHeaders headers = new HttpHeaders();
+            headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=model_" + modelUuid + ".uxf");
+            headers.add(HttpHeaders.CONTENT_TYPE, "application/xml");
+            return new ResponseEntity<>(xmlBytes, headers, HttpStatus.OK);
+        } catch (ModelNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Configuration with id '" + modelUuid + "' not found");
+        } catch (ConfigurationException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("Failed to get configurations");
+        } catch (UxfException e) {
+            return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY)
+                    .body("Failed to export configuration to uxf");
         }
     }
 }

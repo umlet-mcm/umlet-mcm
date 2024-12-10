2c17bbcbbec5162087fc9599c3d5333680bb6826
Benjamin <benjamin.giraud-renard@student.tuwien.ac.at>
2024-12-10 22:05:16 +0100

    add missing import that causes warnings

--- mcm-frontend/src/components/left-side/DialogExport.vue
+++ mcm-frontend/src/components/left-side/DialogExport.vue
@@ -14,6 +14,7 @@ import {Button} from "@/components/ui/button";
 import {exportToUxf} from "@/api/files.ts";
 import {RadioGroup, RadioGroupItem} from "@/components/ui/radio-group";
 import {exportToCsv} from "@/api/graphDB.ts";
+import {Label} from "@/components/ui/label";
 
 // props related
 const props = defineProps({
